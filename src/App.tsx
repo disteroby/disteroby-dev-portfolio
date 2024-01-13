8571e36f9366f04da5bf4e85cdaca875eca0acbc
Roberto Di Stefano <disteroby1502@gmail.com>
2024-01-13 19:13:32 +0100

    Init setup (vite, typescript, tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+          <section className="bg-purple-900 h-dvh flex justify-center items-center">
+              <div className="text-white text-9xl text-center">Hello World</div>
+          </section>
+      </main>
+    </>
+  )
+}
+
+export default App

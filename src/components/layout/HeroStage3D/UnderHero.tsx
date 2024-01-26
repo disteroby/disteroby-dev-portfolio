baf3edf69ce6e963d664f71dcaa4c4c7da2c7cbd
Roberto Di Stefano <disteroby1502@gmail.com>
2024-01-26 02:36:29 +0100

    chore: created grid fading overlay for the section below hero

--- src/components/layout/HeroStage3D/UnderHero.tsx
+++ src/components/layout/HeroStage3D/UnderHero.tsx
@@ -1,43 +1,9 @@
-import { Canvas } from "@react-three/fiber";
-import { Grid } from "@react-three/drei";
-import { useControls } from "leva";
-
 export default function UnderHero() {
-    // const { position, fadeStrength, fadeDistance } = useControls({
-    //     position: {
-    //         value: [0, 0, -5],
-    //     },
-    //     fadeStrength: {
-    //         value: 1,
-    //     },
-    //     fadeDistance: {
-    //         value: 1,
-    //     },
-    // });
-
     return (
-        <div className='absolute w-full'>
-            <Canvas
-                className='h-[100vh!important]'
-                camera={{
-                    position: [0, 0, -10],
-                    fov: 60,
-                }}>
-                <Grid
-                    // rotation-x={Math.PI / 2}
-                    renderOrder={-1}
-                    position={[0, 0, 0]}
-                    infiniteGrid
-                    cellSize={1.139}
-                    cellThickness={0.75}
-                    sectionSize={5.695}
-                    sectionThickness={1}
-                    sectionColor={"#5c6365"}
-                    cellColor={"#7b7d7e"}
-                    fadeDistance={100}
-                    fadeStrength={10}
-                />
-            </Canvas>
+        <div className='absolute w-full h-[200vh] '>
+            <div className='absolute inset-0 bg-gradient-to-b from-black/80 to-dark-gray overflow-hidden' />
+            <div className='absolute inset-0 grid-fade-out' />
+            <div className='absolute inset-0 bg-gradient-to-b to-90% from-transparent to-dark-gray' />
         </div>
     );
 }

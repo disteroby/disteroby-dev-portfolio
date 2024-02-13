1310bcf6a1c5661827e5af2dc40a8c58fd7a8631
Roberto Di Stefano <disteroby1502@gmail.com>
2024-02-13 23:53:34 +0100

    feature: better opacity animation orchestration in Projects Section

--- src/components/UI/ProjectDescription.tsx
+++ src/components/UI/ProjectDescription.tsx
@@ -9,6 +9,9 @@ type ProjectDescriptionProps = {
     index: number;
 };
 
+const descriptionAnimDelay = 0.25;
+const tagAnimDelay = 0.25;
+
 export default function ProjectDescription({
     project,
     index,
@@ -66,10 +69,8 @@ export default function ProjectDescription({
         }
 
         return cta?.map(({ text, type, href }) => (
-            <motion.a
+            <motion.div
                 key={href}
-                href={href}
-                target='_blank'
                 variants={{
                     inView: {
                         opacity: 1,
@@ -78,89 +79,110 @@ export default function ProjectDescription({
                 transition={{
                     duration: 0.65,
                 }}
-                className={twJoin(
-                    "group relative flex gap-2 font-bold opacity-0 transition before:absolute before:left-0 before:right-0 before:top-full before:h-0.5 before:opacity-0 before:transition before:duration-300 before:hover:opacity-100",
-                    isOnTheRight
-                        ? "before:bg-fuchsia-500 hover:text-fuchsia-500"
-                        : "before:bg-cyan-500 hover:text-cyan-500",
-                )}>
-                <span className='mt-0.5'>{getIcon(type)}</span>
-                <span>{text}</span>
-            </motion.a>
+                className='opacity-0'>
+                <a
+                    href={href}
+                    target='_blank'
+                    className={twJoin(
+                        "group relative flex gap-2 font-bold transition duration-300 before:absolute before:left-0 before:right-0 before:top-full before:h-0.5 before:opacity-0 before:transition before:duration-[400ms] before:hover:opacity-100",
+                        isOnTheRight
+                            ? "before:bg-fuchsia-500 hover:text-fuchsia-500"
+                            : "before:bg-cyan-500 hover:text-cyan-500",
+                    )}>
+                    <span className='mt-0.5'>{getIcon(type)}</span>
+                    <span>{text}</span>
+                </a>
+            </motion.div>
         ));
     }
 
     return (
-        <div className='flex h-full flex-col justify-center gap-4 p-4 text-lg'>
-            <div className='-mb-4 flex gap-1 align-bottom opacity-60'>
-                {renderTeamLabel()}
-            </div>
-            <div className='w-fit text-3xl font-light lg:text-5xl'>
-                <span>0{index}. </span>
-                <span className='text-fuchsia-cyan-gradient font-medium'>
-                    {title}
-                </span>
-            </div>
+        <div className='flex h-full flex-col justify-center'>
             <motion.div
+                className='flex flex-col justify-center gap-4 p-4 text-lg'
                 variants={{
                     inView: {
                         transition: {
-                            staggerChildren: 0.2,
-                            delayChildren: 0.4,
+                            staggerChildren: 0.3,
+                            delayChildren: descriptionAnimDelay,
                         },
                     },
                 }}
-                viewport={{ once: true }}
-                whileInView='inView'
-                className={twJoin(
-                    "relative flex flex-col items-stretch gap-6 pl-4 text-justify before:absolute before:bottom-0 before:left-0 before:top-0 before:w-0.5 lg:text-left",
-                    isOnTheRight
-                        ? "before:bg-fuchsia-500"
-                        : "before:bg-cyan-500",
-                )}>
-                {description.map((paragraph, idx) => (
-                    <motion.div
-                        key={idx}
-                        className='text-white/90 opacity-0'
-                        variants={{
-                            inView: {
-                                opacity: 1,
+                viewport={{ once: true, amount: 0.5 }}
+                whileInView='inView'>
+                <motion.div className='-mb-4 flex gap-1 align-bottom opacity-60'>
+                    {renderTeamLabel()}
+                </motion.div>
+                <div className='w-fit text-3xl font-light lg:text-5xl'>
+                    <span>0{index}. </span>
+                    <span className='text-fuchsia-cyan-gradient font-medium'>
+                        {title}
+                    </span>
+                </div>
+                <motion.div
+                    variants={{
+                        inView: {
+                            scaleY: 1,
+                            opacity: 1,
+                            transition: {
+                                staggerChildren: descriptionAnimDelay,
+                                delayChildren: 1,
+                                duration: 1,
                             },
-                        }}
-                        transition={{
-                            duration: 1,
-                        }}>
-                        {paragraph}
-                    </motion.div>
-                ))}
-            </motion.div>
-            <motion.div
-                variants={{
-                    inView: {
-                        transition: {
-                            staggerChildren: 0.25,
-                            delayChildren: 0.75,
                         },
-                    },
-                }}
-                viewport={{ once: true }}
-                whileInView='inView'
-                className='flex flex-wrap justify-center gap-4 lg:justify-start'>
-                {renderTags()}
-            </motion.div>
-            <motion.div
-                variants={{
-                    inView: {
-                        transition: {
-                            staggerChildren: 0.25,
-                            delayChildren: 1.5,
+                    }}
+                    style={{ scaleY: 0, opacity: 0 }}
+                    className={twJoin(
+                        "relative flex origin-top flex-col items-stretch gap-6 pl-4 text-justify before:absolute before:bottom-0 before:left-0 before:top-0 before:w-0.5 lg:text-left",
+                        isOnTheRight
+                            ? "before:bg-fuchsia-500"
+                            : "before:bg-cyan-500",
+                    )}>
+                    {description.map((paragraph, idx) => (
+                        <motion.div
+                            key={idx}
+                            className='text-white/90 opacity-0'
+                            variants={{
+                                inView: {
+                                    opacity: 1,
+                                },
+                            }}
+                            transition={{
+                                duration: 1,
+                            }}>
+                            {paragraph}
+                        </motion.div>
+                    ))}
+                </motion.div>
+                <motion.div
+                    variants={{
+                        inView: {
+                            transition: {
+                                staggerChildren: tagAnimDelay,
+                                delayChildren:
+                                    1 +
+                                    description.length * descriptionAnimDelay,
+                            },
                         },
-                    },
-                }}
-                viewport={{ once: true }}
-                whileInView='inView'
-                className='mt-8 flex flex-wrap justify-center gap-4 md:gap-8 lg:justify-end'>
-                {renderCta()}
+                    }}
+                    className='flex flex-wrap justify-center gap-4 lg:justify-start'>
+                    {renderTags()}
+                </motion.div>
+                <motion.div
+                    variants={{
+                        inView: {
+                            transition: {
+                                staggerChildren: 0.25,
+                                delayChildren:
+                                    1 +
+                                    description.length * descriptionAnimDelay +
+                                    (tags?.length ?? 0) * tagAnimDelay,
+                            },
+                        },
+                    }}
+                    className='mt-8 flex flex-wrap justify-center gap-4 md:gap-8 lg:justify-end'>
+                    {renderCta()}
+                </motion.div>
             </motion.div>
         </div>
     );

f63329506599c4f42a9abe9b8b5115fc0766fd3e
Roberto Di Stefano <disteroby1502@gmail.com>
2024-03-18 00:28:47 +0100

    fix: test on iphone resources limit, part 4

--- src/components/threejs/scenes/HeroStage3D.tsx
+++ src/components/threejs/scenes/HeroStage3D.tsx
@@ -4,12 +4,15 @@ import HeroDevices from "../models/HeroDevices.tsx";
 import HeroGrid from "../models/HeroGrid.tsx";
 import HeroLights from "../models/HeroLights.tsx";
 import PerformanceCanvas from "../utils/PerformanceCanvas.tsx";
+import { isMobileOnly, mobileVendor } from "react-device-detect";
 
 type HeroStage3DProps = {
     inView: boolean;
 };
 
 const HeroStage3D = memo(({ inView }: HeroStage3DProps) => {
+    const isIPhone = isMobileOnly && mobileVendor === "Apple";
+
     return (
         <PerformanceCanvas
             id='main-canvas'
@@ -18,34 +21,33 @@ const HeroStage3D = memo(({ inView }: HeroStage3DProps) => {
                 position: [0, 0.2365, 7.15],
                 fov: 50,
             }}>
-            <group></group>
-            {/*<Environment files='stars.hdr' path='./environment/'>*/}
-            {/*    <Lightformer*/}
-            {/*        intensity={0.01}*/}
-            {/*        rotation-x={Math.PI}*/}
-            {/*        rotation-y={Math.PI}*/}
-            {/*        position={[0, 5, -9]}*/}
-            {/*        scale={[500, 500, 1]}*/}
-            {/*    />*/}
-            {/*</Environment>*/}
-            {/*<fog attach='fog' args={["#0C0C0F", 3, 12]} />*/}
-            {/*<HeroLights />*/}
-            {/*<HeroDevices inView={inView} />*/}
-            {/*<HeroGrid />*/}
-            {/*<mesh position={[0, -1.7, 0]} rotation-x={-Math.PI / 2}>*/}
-            {/*    <planeGeometry args={[50, 50]} />*/}
-            {/*    <meshBasicMaterial color={"#171717"} />*/}
-            {/*</mesh>*/}
-            {/*<group>*/}
-            {/*    <Sparkles*/}
-            {/*        count={40}*/}
-            {/*        scale={[15, 15, 5]}*/}
-            {/*        size={10}*/}
-            {/*        speed={0.15}*/}
-            {/*        opacity={0.1}*/}
-            {/*        color={"#ffffff"}*/}
-            {/*    />*/}
-            {/*</group>*/}
+            <Environment files='stars.hdr' path='./environment/'>
+                <Lightformer
+                    intensity={0.01}
+                    rotation-x={Math.PI}
+                    rotation-y={Math.PI}
+                    position={[0, 5, -9]}
+                    scale={[500, 500, 1]}
+                />
+            </Environment>
+            <fog attach='fog' args={["#0C0C0F", 3, 12]} />
+            <HeroLights />
+            {!isIPhone && <HeroDevices inView={inView} />}
+            <HeroGrid />
+            <mesh position={[0, -1.7, 0]} rotation-x={-Math.PI / 2}>
+                <planeGeometry args={[50, 50]} />
+                <meshBasicMaterial color={"#171717"} />
+            </mesh>
+            <group>
+                <Sparkles
+                    count={40}
+                    scale={[15, 15, 5]}
+                    size={10}
+                    speed={0.15}
+                    opacity={0.1}
+                    color={"#ffffff"}
+                />
+            </group>
         </PerformanceCanvas>
     );
 });

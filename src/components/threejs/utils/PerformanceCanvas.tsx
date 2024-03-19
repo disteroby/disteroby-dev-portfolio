b4542d4bcb16f28bcc285db7a454f44ed106793f
Roberto Di Stefano <disteroby1502@gmail.com>
2024-03-19 19:53:46 +0100

    fix: limit preload for iPhone only

--- src/components/threejs/utils/PerformanceCanvas.tsx
+++ src/components/threejs/utils/PerformanceCanvas.tsx
@@ -1,9 +1,11 @@
 import { Suspense, useRef } from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import { CanvasProps } from "@react-three/fiber/dist/declarations/src/web/Canvas";
+import { Preload } from "@react-three/drei";
 import { EarthMaterial } from "../materials/EarthMaterial.tsx";
 import { ImageFadeMaterial } from "../materials/ImageFadeMaterial.tsx";
 import { useInView } from "framer-motion";
+import { isMobileOnly, mobileVendor } from "react-device-detect";
 
 extend({ ImageFadeMaterial });
 extend({ EarthMaterial });
@@ -12,13 +14,20 @@ export default function PerformanceCanvas({ children, ...props }: CanvasProps) {
     const ref = useRef<HTMLCanvasElement>(null!);
     const isInView = useInView(ref);
 
+    const isIPhone = isMobileOnly && mobileVendor === "Apple";
+
     return (
         <Canvas
-            style={{ contain: "paint" }}
+            style={{
+                contain: "paint",
+            }}
             frameloop={isInView ? "always" : "never"}
             ref={ref}
             {...props}>
-            <Suspense fallback={null}>{children}</Suspense>
+            <Suspense fallback={null}>
+                {children}
+                {!isIPhone && <Preload all />}
+            </Suspense>
         </Canvas>
     );
 }
